ebbbc4c593bbf233c92bb0416e480add5a0eabc9
aabreu <alexia.abreu@adp.com>
2023-01-23 01:27:26 -0300

    codigo aula 3

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const cors = require("cors")
 const rotaLivro = require("./rotas/livro")
+const rotaFavoritos = require("./rotas/favorito")
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: "*"
+}))
 
 app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavoritos)
 
 const port = 8000
 
